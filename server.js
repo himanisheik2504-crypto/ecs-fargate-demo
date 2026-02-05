1aa43edd45cb087510ca74eb4f0a54b7c42be418
Ubuntu <ubuntu@ip-172-31-20-62.ap-southeast-2.compute.internal>
2026-02-05 06:51:40 +0000

    Initial ECS Fargate Node app

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello from ECS Fargate CI/CD!"));
+app.get("/health", (req, res) => res.status(200).send("OK"));
+
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () =>
+  console.log(`App running on port ${port}`)
+);
